f2fde9914761e1bb4eb7348f13d7920a955f695b
Dasutein <dustine_2008@hotmail.com>
2019-06-06 11:05:04 +0800

    Create IDiscordPresence.cs

---
+++ src/DiscordRPC.Main/IDiscordPresence.cs
@@ -0,0 +1,14 @@
+﻿namespace DiscordRPC.Main
+{
+    public interface IDiscordPresence
+    {
+        string discordClientId { get; set; }
+        string discordPresenceState { get; set; }
+        string discordPresenceDetail { get; set; }
+        string discordLargeImageKey { get; set; }
+        string discordLargeImageText { get; set; }
+        string discordSmallImageKey { get; set; }
+        string discordSmallImageText { get; set; }
+        bool useTimeStamp { get; set; }
+    }
+}
