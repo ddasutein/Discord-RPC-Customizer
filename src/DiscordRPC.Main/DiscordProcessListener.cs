f7cbd019dac6167cccc1124848425907db27697f
Dasutein <dustine_2008@hotmail.com>
2019-06-25 14:52:44 +0800

    Rewrite DiscordProcessListener
    Reduced the amount of If-else statements using a switch statement. Also renamed GetDiscordProcess to DiscordProcessListener

---
+++ src/DiscordRPC.Main/DiscordProcessListener.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace DiscordRPC.Main
+{
+    class DiscordProcessListener
+    {
+        private readonly string TAG = "DiscordProcessListener.cs: ";
+        public bool IsDiscordRunning { get; private set; } = false;
+
+        public string DiscordBuildInfo { get; set; }
+
+        public void DiscordProcessName()
+        {
+            Process[] localAll = Process.GetProcesses();
+            foreach (Process process in localAll)
+            {
+#if DEBUG
+                Debug.WriteLine(TAG + process);
+#endif
+
+                switch (process.ProcessName)
+                {
+                    case "Discord":
+                        DiscordBuildInfo = "Stable";
+                        IsDiscordRunning = true;
+                        break;
+                    case "DiscordPTB":
+                        DiscordBuildInfo = "Public Test Beta (PTB)";
+                        IsDiscordRunning = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
