bbf9c9084077d8516b514362491cfcb89e4ce2e6
Dasutein <dustine_2008@hotmail.com>
2019-06-28 13:46:06 +0800

    Refactor AboutWindow.cs

--- src/DiscordRPC.Main/UI/AboutWindow.xaml.cs
+++ src/DiscordRPC.Main/UI/AboutWindow.xaml.cs
@@ -3,50 +3,37 @@ using System.Windows;
 
 namespace DiscordRPC.Main
 {
-    /// <summary>
-    /// Interaction logic for AboutWindow.xaml
-    /// </summary>
     public partial class AboutWindow : Window
     {
 
         // Debug only
-        private string TAG = "AboutWindow.xaml: ";
-
-        // Global variables
-        GetDiscordProcess getDiscordProcess = new GetDiscordProcess();
+        private readonly string TAG = "AboutWindow.xaml: ";
 
         public AboutWindow()
         {
             InitializeComponent();
-
-            // Show application version
-            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            textBlockVersionNumber.Text = "Version: " + version.Remove(version.Length - 2);
-            Debug.WriteLine(TAG + "DiscordRPC version is: " + version);
-
-            // Run Discord process check then display to text block
-            getDiscordProcess.DiscordProcessName();
-
-            #if DEBUG
-            if (getDiscordProcess.DiscordBuildInfo == null)
-            {
-                textBlockDiscordBuildType.Text = "(null)";
-            }
-            else
-            {
-                textBlockDiscordBuildType.Text = getDiscordProcess.DiscordBuildInfo.ToString();
-            }
-            #else
-                textBlockDiscordBuildType.Text = getDiscordProcess.DiscordBuildInfo.ToString();
-            #endif
+            GetApplicationVersionNumber();
+            GetDiscordBuildType();
         }
 
+        private void GetApplicationVersionNumber()
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            textBlockVersionNumber.Text = "Version: " + version.Remove(version.Length - 2);
+#if DEBUG
+            Debug.WriteLine(TAG + "RikoRPC version is: " + version);
+#endif
+        }
+
+        private void GetDiscordBuildType()
+        {
+            DiscordProcessListener getDiscordProcess = new DiscordProcessListener();
+            getDiscordProcess.DiscordProcessName();
+            textBlockDiscordBuildType.Text = getDiscordProcess.DiscordBuildInfo;
+        }
         private void buttonGitHub_Click(object sender, RoutedEventArgs e)
         {
-            // Sends user to GitHub repo
-            var url = "https://github.com/ddasutein/Discord-RPC-csharp";
-            Process.Start(url);
-            Debug.WriteLine(TAG + url);
+            Process.Start("https://github.com/ddasutein/Discord-RPC-csharp");
         }
     }
 }
