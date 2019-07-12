8db960b8211cd1e2f6a7e9541ff2a432b47531d9
Dasutein <dustine_2008@hotmail.com>
2019-07-12 18:40:16 +0800

    Improvement to AboutWindow UI
    Renamed "Contribute on GitHub" button to "View on GitHub". Added new button "Check for Updates". Rewrote methods to get application version number and Discord build. Note: There are some placeholder code for UWP implementation in the future.

--- src/DiscordRPC.Main/UI/AboutWindow.xaml.cs
+++ src/DiscordRPC.Main/UI/AboutWindow.xaml.cs
@@ -8,17 +8,21 @@ namespace DiscordRPC.Main
 
         // Debug only
         private readonly string TAG = "AboutWindow.xaml: ";
+        private bool IsRunningAsUWP = false; // To be changed later
 
         public AboutWindow()
         {
             InitializeComponent();
             GetApplicationVersionNumber();
             GetDiscordBuildType();
+            
+            // Temporary code for the future
+            this.ProjectBuildType.Visibility = Visibility.Collapsed;
         }
 
         private void GetApplicationVersionNumber()
         {
-            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var version = AppUpdateChecker.GetCurrentApplicationVersion();
             textBlockVersionNumber.Text = "Version: " + version.Remove(version.Length - 2);
 #if DEBUG
             Debug.WriteLine(TAG + "RikoRPC version is: " + version);
@@ -27,13 +31,23 @@ namespace DiscordRPC.Main
 
         private void GetDiscordBuildType()
         {
-            DiscordProcessListener getDiscordProcess = new DiscordProcessListener();
-            getDiscordProcess.DiscordProcessName();
-            textBlockDiscordBuildType.Text = getDiscordProcess.DiscordBuildInfo;
+            textBlockDiscordBuildType.Text = DiscordProcessListener.DiscordBuildType;
         }
         private void buttonGitHub_Click(object sender, RoutedEventArgs e)
         {
             Process.Start("https://github.com/ddasutein/Discord-RPC-csharp");
+        }
+
+        private void Button_Check_For_Updates_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsRunningAsUWP)
+            {
+                AppUpdateChecker.ManualCheckVersion();
+            } else
+            {
+                Process.Start("ms-windows-store:");
+            }
+
         }
     }
 }
